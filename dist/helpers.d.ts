50e680108b16ca56d7c461fcb9db1c6d67ca44e5
user.email <eriklm42@gmail.com>
2023-01-02 10:59:04 -0300

    clean dist

--- dist/helpers.d.ts
+++
@@ -1,7 +0,0 @@
-import { BinaryLike } from "crypto";
-export declare const hash: (data: BinaryLike) => string;
-export declare const hashValidated: ({ hash, difficulty, prefix, }: {
-    hash: string;
-    difficulty?: number;
-    prefix?: string;
-}) => boolean;
